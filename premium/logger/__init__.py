b98f41c6698c7beddad9d020f9d8222c10587dde
Harsh Sanghvi <hjain4199@gmail.com>
2022-07-21 13:25:29 +0530

    log updated

--- premium/logger/__init__.py
+++ premium/logger/__init__.py
@@ -18,7 +18,7 @@ LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
 logging.basicConfig(filename = LOG_FILE_PATH,
 filemode = "w",
-format = '[%(asctime)s] - %(levelname)s - %(lineno)d - %(filename)s - %(funcName)s() - %(message)s',
+format = '[%(asctime)s]---%(levelname)s---%(lineno)d---%(filename)s---%(funcName)s()---%(message)s',
 level = logging.INFO
 )
 
@@ -26,7 +26,7 @@ def get_log_dataframe(file_path):
     data = []
     with open(file_path) as log_file:
         for line in log_file.readlines():
-            data.append(line.split("-"))
+            data.append(line.split("---"))
 
     log_df = pd.DataFrame(data)
     columns = ["Time stamp","Log Level","line number","file name","function name","message"]
